01eb5408562447c6aaca461d68673ef9669e31c8
xiaolei <3327716125@qq.com>
2026-03-12 14:50:08 +0800

    add api

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ import traceback
 import requests
 import uvicorn
 from pydantic import BaseModel, Field
-from model import VLMMode
+from model import VLMModel
 
 app = FastAPI(
     title="VLM API",
